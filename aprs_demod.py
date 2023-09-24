900eccfc7b9f03c513c3dd50e08e01405a86fec8
ssmith <stephanelsmith@ymail.com>
2023-09-24 14:26:32 -0500

    wip igate not working?

--- aprs_demod.py
+++ aprs_demod.py
@@ -1,6 +1,7 @@
 #! env/bin/python
 
 import sys
+import os
 import io
 import asyncio
 import struct
@@ -60,44 +61,65 @@ async def read_samples_from_pipe(samples_q,
 async def read_samples_from_rtl_fm(samples_q, 
                                    ):
     try:
+        stderr_task = None
         while True:
-            cmd = 'rtl_fm -f 144.390M -s 22050 -g 10'
-            proc = await asyncio.create_subprocess_exec(
-                cmd.split()[0], *cmd.split()[1:], 
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE
-            )
+            try:
+                cmd = 'rtl_fm -f 144.390M -s 22050 -g 10'
+                proc = await asyncio.create_subprocess_exec(
+                    cmd.split()[0], *cmd.split()[1:], 
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE
+                )
 
-            arr = array('i',range(defs.SAMPLES_SIZE))
-            idx = 0
+                # process stderr messages separately from stdout
+                async def proc_stderr(stderr):
+                    try:
+                        while True:
+                            stderr = await proc.stderr.readline()
+                            msg = stderr.decode().strip()
+                            if msg:
+                                eprint(msg)
+                                #if 'Failed to open rtlsdr device' in msg:
+                                #    os.system('usbreset 0bda:2838')
+                    except Exception as err:
+                        print_exc(err)
+                    except asyncio.CancelledError:
+                        raise
+                # stderr task
+                stderr_task = asyncio.create_task(proc_stderr(proc.stderr))
 
-            while True:
-                stderr = await proc.stderr.readline()
-                if stderr:
-                    msg = stderr.decode().strip()
-                    eprint(msg)
-                    # todo, detect rtl-sdr dongle not found
-                    continue
-                try:
-                    a = await proc.stdout.readexactly(2)
-                except EOFError:
-                    break
-                arr[idx] = struct.unpack('<h', a)[0]
-                # arr[idx] = int.from_bytes(a,'little',signed=True)
-                idx += 1
-                if idx%defs.SAMPLES_SIZE == 0:
-                    if afsk_detector(arr,idx): #afsk signal detector
-                        await samples_q.put((arr, idx))
-                        await asyncio.sleep(0)
-                        arr = array('i',range(defs.SAMPLES_SIZE))
-                    idx = 0
-            await samples_q.put((arr, idx))
-            await asyncio.sleep(0)
+                arr = array('i',range(defs.SAMPLES_SIZE))
+                idx = 0
+                while True:
+                    try:
+                        a = await proc.stdout.readexactly(2)
+                    except EOFError:
+                        eprint('eof')
+                        break
+                    #eprint(a)
+                    arr[idx] = struct.unpack('<h', a)[0]
+                    # arr[idx] = int.from_bytes(a,'little',signed=True)
+                    idx += 1
+                    if idx%defs.SAMPLES_SIZE == 0:
+                        if afsk_detector(arr,idx): #afsk signal detector
+                            await samples_q.put((arr, idx))
+                            await asyncio.sleep(0)
+                            arr = array('i',range(defs.SAMPLES_SIZE))
+                        idx = 0
+                await samples_q.put((arr, idx))
+                await asyncio.sleep(0)
+            finally:
+                eprint('killing rtl_fm process')
+                proc.kill()
+                await proc.wait()
 
     except Exception as err:
         print_exc(err)
     except asyncio.CancelledError:
         raise
+    finally:
+        if stderr_task:
+            stderr_task.cancel()
 
 async def read_samples_from_file(samples_q, 
                                 file,
--- start_igate.sh
+++ start_igate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
+usbreset  0bda:2838
 #rtl_fm -f 144.390M -s 22050 -g 50 -l 0 - | pypy3 aprs_demod.py -t raw - | python aprs_is.py -c KI5TOF-10 -p 17081 -lat 29.699616621630348 -lon -95.75020574442557
-pypy3 aprs_demod.py -t raw rtl_fm | python aprs_is.py -c KI5TOF-10 -p 17081 -lat 29.699616621630348 -lon -95.75020574442557
+python aprs_demod.py -t raw rtl_fm | python aprs_is.py -c KI5TOF-10 -p 17081 -lat 29.699616621630348 -lon -95.75020574442557

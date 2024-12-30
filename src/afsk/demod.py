531ca39ef1b4e0dfa4dbf5135495cb9179300f3c
Stéphane <stephanelsmith@ymail.com>
2024-12-29 18:50:33 -0600

    demod: prev wasnt working ttwr, updated settings work with ttwr and decode 983 frames on tnc test

--- src/afsk/demod.py
+++ src/afsk/demod.py
@@ -51,6 +51,7 @@ class AFSKDemodulator():
         do_memoize = True
         # if options:
             # do_memoize = False
+
         # germany tuned
         # options = dict({
             # 'bandpass_ncoefsbaud' : 3,
@@ -63,11 +64,35 @@ class AFSKDemodulator():
             # 'lpf_aboost'          : 3,
         # }, **options)
 
-        # rtl_fm test tuning
+        # rtl_fm test tuning, working with ttwr
+        # options = dict({
+            # 'bandpass_ncoefsbaud' : 5,
+            # 'bandpass_width'      : 460,
+            # 'bandpass_amark'      : 6,
+            # 'bandpass_aspace'     : 6,
+            # 'lpf_ncoefsbaud'      : 5,
+            # 'lpf_f'               : 1000,
+            # 'lpf_width'           : 360,
+            # 'lpf_aboost'          : 3,
+        # }, **options)
+
+        # optimizer tuning for tnc test, 1020 decoded frames (but NOT ttwr!)
+        # options = dict({
+            # 'bandpass_ncoefsbaud' : 5,
+            # 'bandpass_width'      : 400,
+            # 'bandpass_amark'      : 1,
+            # 'bandpass_aspace'     : 3,
+            # 'lpf_ncoefsbaud'      : 5,
+            # 'lpf_f'               : 800,
+            # 'lpf_width'           : 250,
+            # 'lpf_aboost'          : 3,
+        # }, **options)
+
+        # co-optimize ttwr and tnc test, this is decode ttwr and decode 983 tnc frames
         options = dict({
             'bandpass_ncoefsbaud' : 5,
             'bandpass_width'      : 400,
-            'bandpass_amark'      : 1,
+            'bandpass_amark'      : 2,
             'bandpass_aspace'     : 3,
             'lpf_ncoefsbaud'      : 5,
             'lpf_f'               : 800,
